f435f751b867ceb60070abe381a6225cfd1a75ea
SasidharReddyLingala <sasidharreddilingala@gmail.com>
2020-04-18 10:05:12 +0530

    changes

--- src/main/java/com/base/Base.java
+++ src/main/java/com/base/Base.java
@@ -5,20 +5,24 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.codehaus.plexus.util.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.utils.TestCutomListeners;
+
 public class Base {
 	
 	public static WebDriver driver;
-	
+	static Logger log = Logger.getLogger(Base.class);
 	public static void initialization() {
 		System.setProperty("webdriver.chrome.driver", ".//src//main//resources//Browser_Drivers//chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://www.google.com");
+		log.info("browser lanched.");
 		
 	}
 	
